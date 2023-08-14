4975bdcea11537e66cafbd99616e12ba8e3e3cd3
Jared Bada <official.jared.bada@gmail.com>
2023-08-14 16:41:00 +0300

    vehicle route relationship creation

--- src/vehicle-route/vehicle-route.entity.ts
+++ src/vehicle-route/vehicle-route.entity.ts
@@ -1,6 +1,7 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 
 import { AbstractEntity } from 'src/shared/base-entity';
+import { Vehicle } from 'src/vehicle/vehicle.entity';
 
 @Entity()
 export class VehicleRoute extends AbstractEntity {
@@ -21,4 +22,7 @@ export class VehicleRoute extends AbstractEntity {
 
   @Column({ type: `timestamp` })
   departureTime: Date;
+
+  @ManyToOne(()=> Vehicle,{nullable:false})
+  provider: Vehicle;
 }
